f0f5f7fd26dcf1f623ab2b4b6f3a49b86ff2bee7
unknown <pkb08164@CIS-TELFORD-04.ds.strath.ac.uk>
2013-01-15 14:02:26 +0000

    Added Display library

---
+++ Arduino/libraries/Display/Display.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <Display.h>
+#include <LiquidCrystal.h>
+
+Display::Display(int pin1, int pin2, int pin3, int pin4, int pin5)
+{
+	_lcd = lcd(pin1, pin2, pin3, pin4, pin5);
+}
+
+Display::void setString(char *str)
+{
+	_lcd.clear();
+	_lcd.setCursor(0,1);
+	_lcd.write(s);
+}
+
+Display::void setStringAndBlock(char *str, const int mins)
+{
+	setString(str);
+	delay(mins * 1000);
+}
+
+
+
+	
