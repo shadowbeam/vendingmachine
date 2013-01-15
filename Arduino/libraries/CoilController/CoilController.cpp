24c3bd6598d381e99329d04ae0a84f82301f48e8
unknown <pkb08164@CIS-TELFORD-04.ds.strath.ac.uk>
2013-01-15 15:59:15 +0000

    Added CoilController

---
+++ Arduino/libraries/CoilController/CoilController.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <CoilController.h>
+
+
+CoilController::CoilController()
+{
+	//TODO setup for motors
+}
+
+void CoilController::vend()
+{
+	//TODO vend action
+}
---
+++ Arduino/libraries/CoilController/CoilController.h
@@ -0,0 +1,14 @@
+#ifndef Coil_h
+#define Coil_h
+
+#include <Arduino.h>
+
+
+class CoilController
+{
+  public:
+    CoilController();
+    void vend();
+};
+
+#endif
