c7b854b26ca2165203ec876bf0787a6730454e56
unknown <pkb08164@CIS-TELFORD-04.ds.strath.ac.uk>
2013-01-15 14:55:27 +0000

    Fixed display library syntax error

--- Arduino/libraries/Display/Display.cpp
+++ Arduino/libraries/Display/Display.cpp
@@ -7,19 +7,15 @@ Display::Display(int pin1, int pin2, int pin3, int pin4, int pin5)
 	_lcd = lcd(pin1, pin2, pin3, pin4, pin5);
 }
 
-Display::void setString(char *str)
+void Display::setString(char *str)
 {
 	_lcd.clear();
 	_lcd.setCursor(0,1);
 	_lcd.write(s);
 }
 
-Display::void setStringAndBlock(char *str, const int mins)
+void Display::setStringAndBlock(char *str, const int mins)
 {
 	setString(str);
 	delay(mins * 1000);
 }
-
-
-
-	
